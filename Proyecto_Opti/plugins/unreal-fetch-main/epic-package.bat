738ce0cc5950b8f665fd4525f36a893f35390abb
ioserakbar <ioserakbar@gmail.com>
2022-05-19 04:00:01 -0500

    fixed widgets and flag bases

---
+++ Proyecto_Opti/plugins/unreal-fetch-main/epic-package.bat
@@ -0,0 +1,3 @@
+echo "Epic Packaging ..."
+
+"D:\Programs\Epic Games\UE_%1\Engine\Build\BatchFiles\RunUAT.bat" BuildPlugin -Plugin=D:\Repos\GDi4K\unreal-fetch\UnrealFetchDevApp\Plugins\Fetch\Fetch.uplugin -Package=ToPackage -Rocket
---
+++ Proyecto_Opti/plugins/unreal-fetch-main/package.bat
@@ -0,0 +1,18 @@
+rmdir /S /Q ToPackage
+
+md ToPackage
+md ToPackage\Fetch
+
+xcopy UnrealFetchDevApp\Plugins\Fetch\Fetch.uplugin ToPackage\Fetch
+
+md ToPackage\Fetch\Config
+xcopy UnrealFetchDevApp\Plugins\Fetch\Config ToPackage\Fetch\Config /E/H
+
+md ToPackage\Fetch\Resources
+xcopy UnrealFetchDevApp\Plugins\Fetch\Resources ToPackage\Fetch\Resources /E/H
+
+md ToPackage\Fetch\Source
+xcopy UnrealFetchDevApp\Plugins\Fetch\Source ToPackage\Fetch\Source /E/H
+
+md ToPackage\Fetch\Content
+xcopy UnrealFetchDevApp\Plugins\Fetch\Content ToPackage\Fetch\Content /E/H
